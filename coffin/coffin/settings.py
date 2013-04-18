9031a9332fef212192f5a61edf25f6190aa54774
Tim Schurig <T.schurig@gmail.com>
2013-04-18 21:08:33 +0200

    URLconf and views

--- coffin/coffin/settings.py
+++ coffin/coffin/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/Schurock/mytemplates/',
+    '/Users/Schurock/Repositories/coffin_poll/coffin/mytemplates/',
 )
 
 INSTALLED_APPS = (
